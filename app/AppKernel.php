03eb8daeaacbe3039758834aaf0c36f81c3a77b7
IgorTokman <igortokman1994@ukr.net>
2016-04-15 22:35:32 +0300

    added new bundles: ModelBundle and CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Blog\CoreBundle\CoreBundle(),
+            new ModelBundle\ModelBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

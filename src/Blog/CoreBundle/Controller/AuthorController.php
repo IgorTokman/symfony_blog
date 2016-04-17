b3e061d74cab5de5158fb764d0c0fcb282da37ef
IgorTokman <igortokman1994@ukr.net>
2016-04-17 13:23:13 +0300

    created the showAction in the AuthorController class. realized the test showauthor

--- src/Blog/CoreBundle/Controller/AuthorController.php
+++ src/Blog/CoreBundle/Controller/AuthorController.php
@@ -8,13 +8,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class AuthorController extends Controller
 {
     /**
-     * @Route("/show")
+     * Show posts by author
+     * @param string slug
+     * @Route("/author/{slug}")
+     * @Template()
+     * @return array
      */
-    public function showAction()
+    public function showAction($slug)
     {
-        return $this->render('CoreBundle:Author:show.html.twig', array(
-            // ...
-        ));
+        $author = $this->getDoctrine()
+            ->getRepository('ModelBundle:Author')
+            ->findOneBy(array('slug' => $slug));
+
+        if($author === null)
+            throw $this->createNotFoundException('Author was not found');
+
+        $posts = $this->getDoctrine()->getRepository('ModelBundle:Post')
+                    ->findBy( array(
+                              'author' => $author
+                          )
+                    );
+
+        return array(
+                'author' => $author,
+                'posts' => $posts
+             );
     }
 
 }
--- tests/Blog/CoreBundle/Controller/AuthorControllerTest.php
+++ tests/Blog/CoreBundle/Controller/AuthorControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Blog\CoreBundle\Tests\Controller;
 
+use Blog\ModelBundle\Entity\Author;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -17,7 +18,20 @@ class AuthorControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/show');
+        /**@var Author $author**/
+        $author = $client->getContainer()->get('doctrine')
+                    ->getManager()->getRepository('ModelBundle:Author')
+                    ->findFirst();
+
+        $authorPostsCount = $author->getPosts()->count();
+
+        $crawler = $client->request('GET', '/author' . $author->getSlug());
+
+        $this->assertTrue($client->getResponse()->isSuccessful(),
+            "The response was not successful");
+
+        $this->assertCount($authorPostsCount, $crawler->filter('h2'),
+                    'There should be ' . $authorPostsCount . ' posts');
     }
 
 }
